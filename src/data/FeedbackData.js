6fdaf9a0cb48fca8ccb5c228b92edae82d2946cb
nick-yelle <yellenicholas@gmail.com>
2022-04-28 10:06:28 -0400

    28Apr2022 Form Validation, Rating Select

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = [
+  {
+  id: 1,
+  rating: 10,
+  text: 'I am here writing to this file.'
+  },
+  {
+    id: 2,
+    rating: 4,
+    text: 'Woah, woah, woah. Hello there sir.'
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: 'How are you doing today?'
+  }
+]
+
+export default FeedbackData
+
